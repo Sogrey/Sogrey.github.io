e512c61494485c0b5d145d0081456d996dd88e7f
sogrey <suzhaoheng2006@163.com>
2018-11-02 11:04:43 +0800

    Site updated: 2018-11-02 11:04:06

--- js/init.js
+++ js/init.js
@@ -1,4 +1,4 @@
-/**
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>/**
  * author: Sogrey
  * date：2017/5/4
  */
