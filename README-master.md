930dfc8255e38d9a6a82778f12f2e20a6a5f3664
Sogrey <suzhaoheng2006@163.com>
2019-04-07 18:50:57 +0800

    update cmd.bat

---
+++ README-master.md
@@ -0,0 +1,9 @@
+# Sogrey.github.io
+
+[![](https://sogrey.github.io/img/logo/sogrey_logo.svg)](https://sogrey.github.io/)
+
+Sogrey's github pages
+
+
+[点此预览](https://sogrey.github.io/)
+
--- cmd.bat
+++ cmd.bat
@@ -12,6 +12,7 @@ echo https://sogrey.github.io/
 echo ~
 echo 请选择：
 echo =================================
+echo 【0】 初始化
 echo 【1】 hexo g
 echo 【2】 hexo s
 echo 【3】 hexo d
@@ -21,31 +22,36 @@ echo.
 echo 请输入执行的批处理命令序号。
 echo. 
 set /p lc= ★命令★：
+if %lc%==0 goto init
 if %lc%==1 goto g
 if %lc%==2 goto s
 if %lc%==3 goto d
 if %lc%==q goto e 
 pause
 
+:init
+start npm install
+goto start
+
 :g
 start hexo g
 goto start
 
 :s
-copy E:\workspace\github\sogrey\sogrey.github.io\404.html E:\workspace\github\sogrey\sogrey.github.io\public\404.html
-copy E:\workspace\github\sogrey\sogrey.github.io\baidu_verify_hTO3FZGPz7.html E:\workspace\github\sogrey\sogrey.github.io\public\baidu_verify_hTO3FZGPz7.html
-copy E:\workspace\github\sogrey\sogrey.github.io\README.md E:\workspace\github\sogrey\sogrey.github.io\public\README.md
-copy E:\workspace\github\sogrey\sogrey.github.io\weichat.html E:\workspace\github\sogrey\sogrey.github.io\public\about\weichat.html
-copy E:\workspace\github\sogrey\sogrey.github.io\sogrey.html E:\workspace\github\sogrey\sogrey.github.io\public\about\sogrey.html
+copy 404.html public\404.html
+copy baidu_verify_hTO3FZGPz7.html public\baidu_verify_hTO3FZGPz7.html
+copy README-master.md public\README.md
+copy weichat.html public\about\weichat.html
+copy sogrey.html public\about\sogrey.html
 start hexo s
 goto start
 
 :d
-copy E:\workspace\github\sogrey\sogrey.github.io\404.html E:\workspace\github\sogrey\sogrey.github.io\public\404.html
-copy E:\workspace\github\sogrey\sogrey.github.io\baidu_verify_hTO3FZGPz7.html E:\workspace\github\sogrey\sogrey.github.io\public\baidu_verify_hTO3FZGPz7.html
-copy E:\workspace\github\sogrey\sogrey.github.io\README.md E:\workspace\github\sogrey\sogrey.github.io\public\README.md
-copy E:\workspace\github\sogrey\sogrey.github.io\weichat.html E:\workspace\github\sogrey\sogrey.github.io\public\about\weichat.html
-copy E:\workspace\github\sogrey\sogrey.github.io\sogrey.html E:\workspace\github\sogrey\sogrey.github.io\public\about\sogrey.html
+copy 404.html public\404.html
+copy baidu_verify_hTO3FZGPz7.html public\baidu_verify_hTO3FZGPz7.html
+copy README-master.md public\README.md
+copy weichat.html public\about\weichat.html
+copy sogrey.html public\about\sogrey.html
 start hexo d
 goto start
 
