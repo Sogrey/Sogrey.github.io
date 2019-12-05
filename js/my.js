beffcd78417ddcaf9897ea20c7bf83812d42e224
sogrey <suzhaoheng2006@163.com>
2019-12-05 22:20:35 +0800

    Site updated: 2019-12-05 22:20:29

--- js/my.js
+++ js/my.js
@@ -8,7 +8,7 @@ var SogreyJs = (function () {
                                 'padding:15px 200px;background:url(https://sogrey.github.io/img/logo/sogrey.github.io.svg) no-repeat;height:1px;line-height:250px;',
                                 'text-shadow:1px 1px 5px rgba(0,0,0.2);font-size:20px;',
                                 'text-shadow:1px 1px 3px rgba(0,0,0.2);font-size:40px;',
-                                'font-size:30px;font-weight: 500;color: #3eaf7c !important;text-decoration: none;'
+                                'font-size:25px;font-weight: 500;color: #3eaf7c !important;text-decoration: none;'
                         );
                 } catch (e) {}
         }
