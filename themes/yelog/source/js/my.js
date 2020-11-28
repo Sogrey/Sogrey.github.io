be24f5de9a67e899212f4022a436ea5f1d54c051
Sogrey <suzhaoheng2006@163.com>
2020-11-28 15:39:51 +0800

    deploy dev

--- themes/yelog/source/js/my.js
+++ themes/yelog/source/js/my.js
@@ -2,7 +2,7 @@ var SogreyJs = (function () {
 
         var clearConsole = function () {
                 try {
-                        window.console && window.console.clear && console.clear();
+                        // window.console && window.console.clear && console.clear();
                         window.console && window.console.log && console.log('%c %c\nWelcome to%c\nSogrey\'s Blog\n%chttps://sogrey.github.io \n',
 
                                 'padding:15px 200px;background:url(https://sogrey.github.io/img/logo/sogrey.github.io.svg) no-repeat;height:1px;line-height:250px;',
