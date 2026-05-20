0da220fc0fdc5f27521ac9fc960ea4e3dc8845d3
sogrey <suzhaoheng2006@163.com>
2026-05-21 02:15:34 +0800

    fix: 修复控制台背景图片样式
    - 添加 font-size: 1px 确保背景图片有渲染空间
    - 简化样式属性

--- themes/yelog/source/js/my.js
+++ themes/yelog/source/js/my.js
@@ -1,1 +1,24 @@
-var SogreyJs=function(){var n=function(){try{window.console&&window.console.clear&&console.clear(),window.console&&window.console.log&&console.log("%c %c\nWelcome to%c\nSogrey's Blog\n%chttps://sogrey.top \n","padding:50px 200px;background:url(https://sogrey.top/img/logo/sogrey.github.io.svg) no-repeat center center;background-size:contain;height:100px;line-height:100px;","text-shadow:1px 1px 5px rgba(0,0,0.2);font-size:20px;","text-shadow:1px 1px 3px rgba(0,0,0.2);font-size:40px;","font-size:25px;font-weight:500;color:#3eaf7c!important;text-decoration:none;")}catch(o){}};return{init:function(){n()}}}();window.onload=function(){SogreyJs.init()};
+var SogreyJs = (function () {
+
+        var clearConsole = function () {
+                try {
+                        window.console && window.console.clear && console.clear();
+                        window.console && window.console.log && console.log('%c %c\nWelcome to%c\nSogrey\'s Blog\n%chttps://sogrey.top \n',
+
+                                'font-size: 1px; padding: 50px 200px; background: url(https://sogrey.top/img/logo/sogrey.github.io.svg) no-repeat center; background-size: contain;',
+                                'text-shadow: 1px 1px 5px rgba(0,0,0.2); font-size: 20px;',
+                                'text-shadow: 1px 1px 3px rgba(0,0,0.2); font-size: 40px;',
+                                'font-size: 25px; font-weight: 500; color: #3eaf7c !important; text-decoration: none;'
+                        );
+                } catch (e) {}
+        }
+
+        return {
+                init: function () {
+                        clearConsole();
+                }
+        }
+})();
+window.onload = function () {
+        SogreyJs.init();
+}
