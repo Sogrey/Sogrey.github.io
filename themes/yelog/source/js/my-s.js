d7131bf36543f65d5a7adf6f1248f3c7211e954d
sogrey <suzhaoheng2006@163.com>
2026-05-21 02:02:11 +0800

    fix: 修复控制台背景图片不显示问题
    - 将 height 从 1px 改为 100px
    - 添加 background-size: contain 确保图片完整显示
    - 添加 center center 定位
    - 更新域名为 sogrey.top

--- themes/yelog/source/js/my-s.js
+++ themes/yelog/source/js/my-s.js
@@ -3,9 +3,9 @@ var SogreyJs = (function () {
         var clearConsole = function () {
                 try {
                         window.console && window.console.clear && console.clear();
-                        window.console && window.console.log && console.log('%c %c\nWelcome to%c\nSogrey\'s Blog\n%chttps://sogrey.github.io \n',
+                        window.console && window.console.log && console.log('%c %c\nWelcome to%c\nSogrey\'s Blog\n%chttps://sogrey.top \n',
 
-                                'padding:15px 200px;background:url(https://sogrey.github.io/img/logo/sogrey.github.io.svg) no-repeat;height:1px;line-height:250px;',
+                                'padding:50px 200px;background:url(https://sogrey.top/img/logo/sogrey.github.io.svg) no-repeat center center;background-size:contain;height:100px;line-height:100px;',
                                 'text-shadow:1px 1px 5px rgba(0,0,0.2);font-size:20px;',
                                 'text-shadow:1px 1px 3px rgba(0,0,0.2);font-size:40px;',
                                 'font-size:25px;font-weight: 500;color: #3eaf7c !important;text-decoration: none;'
